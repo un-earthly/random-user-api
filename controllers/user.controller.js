5684bbf00a1ec0ca2cb7133f9b46ad1e9435a387
MD.Alamin <88502576+un-earthly@users.noreply.github.com>
2022-09-03 09:25:39 -0700

    random user on each request

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,28 +1,28 @@
-// const jsonFile = require("")
 const fs = require("fs")
 module.exports.getAllUsers = (req, res) => {
-    const { count } = req.query;
+    // const { count } = req.query;
     fs.readFile('data/user.json', 'utf8', (err, data) => {
-        if (!err) {
+        // if (!err) {
 
-            if (!req.query) {
-                res.status(200).send(data)
-            }
-            console.log(JSON.parse(data))
-        }
-        else {
-            res.status(500).send({ err })
-        }
+        //     if (!req.query) {
+        // res.status(200).send(data)
+        //     }
+        //     console.log(JSON.parse(data))
+        // }
+        // else {
+        //     res.status(500).send({ err })
+        // }
 
+        !err ? res.status(200).send(data) : res.status(500).send({ err })
 
     })
 }
-module.exports.getRandomUser = (req, res, next) => {
+module.exports.getRandomUser = (req, res) => {
 
     fs.readFile('data/user.json', 'utf8', (err, data) => {
-        // !err ? res.status(200).send(data) : res.status(500).send({ err })
-
-        console.log({ random: "user" })
+        const dataArray = JSON.parse(data)
+        const random = Math.floor(Math.random() * (dataArray.length))
+        res.send({ random: dataArray[random] })
 
     })
 }
