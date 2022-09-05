12a68a381e931ee3ebb5aadf874bda65ff17f9e5
MD.Alamin <88502576+un-earthly@users.noreply.github.com>
2022-09-05 05:01:48 -0700

    structure change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 module.exports.getAllUsers = (req, res) => {
     // const { count } = req.query;
-    fs.readFile('data/user.json', 'utf8', (err, data) => {
+    fs.readFile('user.json', 'utf8', (err, data) => {
         // if (!err) {
 
         //     if (!req.query) {
@@ -18,7 +18,7 @@ module.exports.getAllUsers = (req, res) => {
 }
 module.exports.getRandomUser = (req, res) => {
 
-    fs.readFile('data/user.json', 'utf8', (err, data) => {
+    fs.readFile('user.json', 'utf8', (err, data) => {
         const { users } = JSON.parse(data)
         const random = Math.floor(Math.random() * (users.length))
         res.send({ random: users[random] })
@@ -26,10 +26,10 @@ module.exports.getRandomUser = (req, res) => {
     })
 }
 module.exports.addUser = (req, res, next) => {
-    fs.readFile('data/user.json', 'utf8', (err, data) => {
+    fs.readFile('user.json', 'utf8', (err, data) => {
         let dataArray = JSON.parse(data);
         dataArray['users'].push(req.body);
-        fs.writeFile("data/user.json", JSON.stringify(dataArray), (err) => {
+        fs.writeFile("user.json", JSON.stringify(dataArray), (err) => {
             !err ? res.send(dataArray) : res.send(err)
         })
 
@@ -43,7 +43,7 @@ module.exports.updateUser = (req, res, next) => {
         return res.status(400).send({ err: 'No Data found in request body to update User' })
     }
 
-    fs.readFile('data/user.json', 'utf8', (err, data) => {
+    fs.readFile('user.json', 'utf8', (err, data) => {
         let dataArray = JSON.parse(data);
         const users = dataArray['users'];
         const user = users.find(user => user.id === pid);
@@ -66,7 +66,7 @@ module.exports.updateUser = (req, res, next) => {
 
         if (userIndex !== -1) {
             users[userIndex] = user;
-            fs.writeFile("data/user.json", JSON.stringify(dataArray), (err) => {
+            fs.writeFile("user.json", JSON.stringify(dataArray), (err) => {
                 !err ? res.send(dataArray) : res.send(err)
             })
         }
@@ -81,7 +81,7 @@ module.exports.bulkUpdateUser = (req, res, next) => {
 
     const usersIdsArray = toUpdate.map(user => user.id)
     if (!usersIdsArray) return res.status(411).send({ err: 'No Id found in request body to update Users' })
-    fs.readFile('data/user.json', 'utf8', (err, data) => {
+    fs.readFile('user.json', 'utf8', (err, data) => {
         let dataArray = JSON.parse(data);
         const users = dataArray['users'];
         const filteredUsers = users.filter(function (user) {
@@ -93,7 +93,7 @@ module.exports.bulkUpdateUser = (req, res, next) => {
 
 
         toUpdate.map((user, index) => {
-            
+
             if (user.gender) {
                 filteredUsers[index].gender = user.gender
             }
@@ -116,7 +116,7 @@ module.exports.bulkUpdateUser = (req, res, next) => {
             for (let i = 0; i < usersIndex.length; i++) {
                 usersIndex[i] = filteredUsers[i]
             }
-            fs.writeFile("data/user.json", JSON.stringify(dataArray), (err) => {
+            fs.writeFile("user.json", JSON.stringify(dataArray), (err) => {
                 !err ? res.send(dataArray) : res.send(err)
             })
 
@@ -129,12 +129,12 @@ module.exports.bulkUpdateUser = (req, res, next) => {
 module.exports.deleteUser = (req, res, next) => {
     const pid = req.params.id
 
-    fs.readFile('data/user.json', 'utf8', (err, data) => {
+    fs.readFile('user.json', 'utf8', (err, data) => {
         let dataArray = JSON.parse(data);
         const users = dataArray['users']
         const newUsers = users.filter(user => user.id !== pid);
         dataArray = newUsers
-        fs.writeFile("data/user.json", JSON.stringify(dataArray), (err) => {
+        fs.writeFile("user.json", JSON.stringify(dataArray), (err) => {
             !err ? res.send(dataArray) : res.send(err)
         })
 
