ad7adda354bbc433cd36c0fafb95a046c5c286f2
MD.Alamin <88502576+un-earthly@users.noreply.github.com>
2022-09-06 01:30:52 -0700

    trying different user method

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const userRoute = require("./routes/v1/user.route")
 require("dotenv").config()
 app.use(cors())
 app.use(express.json())
-app.use("/v1/user", userRoute)
 
 app.get("/", (req, res) => {
     res.send({ "Avaiable get Routes ": " v1/user/random && v1/user/all" })
 })
+
+app.use("/v1/user", userRoute)
 app.listen(process.env.HOST || 80, () => {
     console.log(`server is running in ${process.env.HOST}`)
 })
