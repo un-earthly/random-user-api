39def00570ff1b8c87c2b4a46d757dfcf7a42c56
MD.Alamin <88502576+un-earthly@users.noreply.github.com>
2022-09-06 07:32:46 -0700

    routes added controlling from controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,37 @@
 const fs = require("fs")
+module.exports.landing = (req, res) => {
+    res.send(
+        ` <div style="font-size:25px; ">
+    <h1>Random User Generator API</h1>
+    <p>Here are the available Get routes</p>
+    <ol>
+      <li>GET : <a href="/">Home</a></li>
+      <li>GET : <a href="api/v1/user/random">Random User</a></li>
+      <li>GET : <a href="api/v1/user/all">All Users</a></li>
+    </ol> 
+    <p>
+    Here are the other available routes in this api
+    </p>
+    <ul>
+        <li>
+        POST : /api/v1/user/save
+        </li>
+        <li>
+        PATCH : /api/v1/user/update/:id
+        </li>
+        <li>
+        PATCH : /api/v1/user/bulk-update/
+        </li>
+        <li>
+        DELETE : /api/v1/user/delete/:id
+        </li>
+    </ul>
+    </div>
+    `
+    );
+};
 module.exports.getAllUsers = (req, res) => {
-    // const { count } = req.query;
     fs.readFile('user.json', 'utf8', (err, data) => {
-        // if (!err) {
-
-        //     if (!req.query) {
-        // res.status(200).send(data)
-        //     }
-        //     console.log(JSON.parse(data))
-        // }
-        // else {
-        //     res.status(500).send({ err })
-        // }
 
         !err ? res.status(200).send(data) : res.status(500).send({ err })
     })
@@ -25,25 +45,28 @@ module.exports.getRandomUser = (req, res) => {
 
     })
 }
-module.exports.addUser = (req, res, next) => {
+module.exports.addUser = (req, res) => {
     fs.readFile('user.json', 'utf8', (err, data) => {
+        if (err) return res.status(400).send({ err })
         let dataArray = JSON.parse(data);
+        req.body.id = require('crypto').randomBytes(12).toString('hex');
         dataArray['users'].push(req.body);
         fs.writeFile("user.json", JSON.stringify(dataArray), (err) => {
-            !err ? res.send(dataArray) : res.send(err)
+            !err ? res.send(dataArray) : res.send({ err })
         })
 
     })
 
 }
-module.exports.updateUser = (req, res, next) => {
+module.exports.updateUser = (req, res) => {
     const { gender, name, contact, address, photoUrl } = req.body
     const pid = req.params.id
     if (Object.entries(req.body).length === 0) {
-        return res.status(400).send({ err: 'No Data found in request body to update User' })
+        return res.status(400).send({ err })
     }
 
     fs.readFile('user.json', 'utf8', (err, data) => {
+        if (err) return res.status(400).send({ err })
         let dataArray = JSON.parse(data);
         const users = dataArray['users'];
         const user = users.find(user => user.id === pid);
@@ -82,6 +105,8 @@ module.exports.bulkUpdateUser = (req, res) => {
     const usersIdsArray = toUpdate.map(user => user.id)
     if (!usersIdsArray) return res.status(411).send({ err: 'No Id found in request body to update Users' })
     fs.readFile('user.json', 'utf8', (err, data) => {
+        if (err) return res.status(400).send({ err })
+
         let dataArray = JSON.parse(data);
         const users = dataArray['users'];
         const filteredUsers = users.filter(function (user) {
@@ -130,6 +155,8 @@ module.exports.deleteUser = (req, res) => {
     const pid = req.params.id
 
     fs.readFile('user.json', 'utf8', (err, data) => {
+        if (err) return res.status(400).send({ err })
+
         let dataArray = JSON.parse(data);
         const users = dataArray['users']
         const newUsers = users.filter(user => user.id !== pid);
